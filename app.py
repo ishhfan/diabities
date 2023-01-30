1169ebb96f5572f73c7221e53c43fffa3c7f0e2e
Ishrath <ishrath2405@gmail.com>
2023-01-31 04:36:41 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+import pickle
+app=Flask(__name__)
+#load the module
+model=pickle.load(open('savedmodel.sav','rb'))
+@app.route('/')
+def home1():
+    result = " "
+    return render_template('home1.html',**locals())
+@app.route('/predict',methods=['POST','GET'])
+
+def predict():
+    Pregnancies=float(request.form['Pregnancies'])
+    Glucose=float(request.form['Glucose'])
+    BloodPressure=float(request.form['BloodPressure'])
+    SkinThickness=float(request.form['SkinThickness'])
+    Insulin=float(request.form['Insulin'])
+    BMI=float(request.form['BMI'])
+    DiabetesPedigreeFunction=float(request.form['DiabetesPedigreeFunction'])
+    Age=float(request.form['Age'])
+    result  = model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])[0]
+    return render_template('home1.html',**locals())
+
+if __name__ == '__main__':
+    app.run(debug=True)
